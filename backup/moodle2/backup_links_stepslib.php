4dc61cce03fa8a944c35df3b5767406e3e988243
sbourget <>
2013-06-07 23:43:33 +0000

    Initial Implementation of Backup code (Restore code pending)

---
+++ backup/moodle2/backup_links_stepslib.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * This block generates a simple list of links based on the users 
+ * department association
+ *
+ * @package   block_links
+ * @copyright 2013 Stephen Bourget
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class backup_links_block_structure_step extends backup_block_structure_step {
+
+    protected function define_structure() {
+        global $DB;
+
+        // Define each element separated
+
+        $link = new backup_nested_element('link', null, array(
+            'id','linktext', 'url', 'notes', 'defaultshow', 'department'));
+
+        // Define sources
+
+        $link->set_source_sql("SELECT * FROM {block_links}", array());
+
+        // Annotations (none)
+
+        // Return the root element (links), wrapped into standard block structure
+        return $this->prepare_block_structure($link);
+    }
+}
