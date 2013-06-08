258a548cca955c65a4586947a62103364709e767
sbourget <>
2013-06-08 00:26:01 +0000

    Initial Implementation of Restore code

---
+++ backup/moodle2/restore_links_block_task.class.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * This block generates a simple list of links based on the users 
+ * department association
+ *
+ * @package   block_links
+ * @copyright 2013 Stephen Bourget
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once($CFG->dirroot . '/blocks/links/backup/moodle2/restore_links_stepslib.php'); // We have structure steps
+
+class restore_links_block_task extends restore_block_task {
+
+    protected function define_my_settings() {
+    }
+
+    protected function define_my_steps() {
+        // rss_client has one structure step
+        $this->add_step(new restore_links_block_structure_step('links_structure', 'links.xml'));
+    }
+
+    public function get_fileareas() {
+        return array(); // No associated fileareas
+    }
+
+    public function get_configdata_encoded_attributes() {
+        return array(); // No special handling of configdata
+    }
+
+    static public function define_decode_contents() {
+        return array();
+    }
+
+    static public function define_decode_rules() {
+        return array();
+    }
+}
---
+++ backup/moodle2/restore_links_stepslib.php
@@ -0,0 +1,77 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * This block generates a simple list of links based on the users 
+ * department association
+ *
+ * @package   block_links
+ * @copyright 2013 Stephen Bourget
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+* Define all the restore steps that wll be used by the restore_links_block_task
+*/
+
+/**
+* Define the complete links structure for restore
+*/
+class restore_links_block_structure_step extends restore_structure_step {
+
+    protected function define_structure() {
+
+        $paths = array();
+
+        $paths[] = new restore_path_element('block', '/block', true);
+        $paths[] = new restore_path_element('link', '/block/link');
+
+        return $paths;
+    }
+
+    public function process_block($data) {
+        global $DB;
+
+        $data = (object)$data;
+
+        // For any reason (non multiple, dupe detected...) block not restored, return
+        if (!$this->task->get_blockid()) {
+            return;
+        }
+
+        // Iterate over all the link elements, creating them if needed
+        if (isset($data->link)) {
+            foreach ($data->link as $link) {
+                $link = (object)$link;
+                // Look if the same link is available by url
+                $select = 'url = :url AND linktext = :linktext';
+                $params = array('url' => $link->url, 'linktext' => $link->linktext);
+                
+                
+                // The link already exists, use it
+                if ($linkid = $DB->get_field_select('block_links', 'id', $select, $params, IGNORE_MULTIPLE)) {
+                    // The record exists (Do nothing)!
+
+                // The link doesn't exist, create it
+                } else {
+                    $linkid = $DB->insert_record('block_links', $link);
+                    
+                }
+            }
+        }
+    }
+}
