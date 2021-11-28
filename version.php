6850cea8bdb0ce3fa3852eb007471e23b18b2647
Stephen Bourget <Stephen.Bourget@sau19.org>
2021-11-28 10:42:11 -0500

    block_links: bump version for 4.0

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021042600;
-$plugin->requires = 2021042300;
+$plugin->version = 2021112800;
+$plugin->requires = 2021112600;
 $plugin->component = 'block_links';
 
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "M3.11 (2021042600)";  // User-friendly version number.
+$plugin->release = "M4.0 (2021112800)";  // User-friendly version number.
